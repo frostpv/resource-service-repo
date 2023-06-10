ae155a77de69772bfbb2571101e1d1b61eebaaee
vasilijpetecuk <frostpv@gmail.com>
2023-06-10 19:36:29 +0300

    [BE][TSK-5] - added eureka

--- src/main/java/com/songs/songsservice/SongsServiceApplication.java
+++ src/main/java/com/songs/songsservice/SongsServiceApplication.java
@@ -2,8 +2,10 @@ package com.songs.songsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SongsServiceApplication {
 
 	public static void main(String[] args) {
